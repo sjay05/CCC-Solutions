11bc6ebdaa840c81e9c4435f8ed99252b0ccb938
sjay05 <anusha.perumal@gmail.com>
2019-12-23 16:35:39 -0500

    j3

---
+++ 2016/J3.py
@@ -0,0 +1,10 @@
+s = raw_input()
+lengths = []
+
+for i in range(len(s)):
+    for j in range(i, len(s)):
+        s2 = [i:j+1]
+        if s2 == s2[::-1]:
+            lengths.append(len(s2))
+
+print sorted(lengths)[len(lengths)-1]
