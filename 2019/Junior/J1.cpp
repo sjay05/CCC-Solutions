ea97b0a0eac4a86d481e3a473bfc580cd06aa64a
sjay05 <anusha.perumal@gmail.com>
2019-12-28 13:47:36 -0500

    cpp version of 2019 J1

---
+++ 2019/Junior/J1.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a1, b1, a2, b2, a3, b3;
+    scanf("%d", &a3); scanf("%d", &a2); scanf("%d", &a1); scanf("%d", &b3); scanf("%d", &b2); scanf("%d", &b1);
+    int a_total, b_total;
+    a_total = a3*3 + a2*2 + a1;  
+    b_total = b3*3 + b2*2 + b1;
+    if (a_total == b_total) printf("T\n");
+    if (a_total > b_total) printf("A\n");
+    if (a_total < b_total) printf("B\n");
+}
