3191fd0e07355f21c767ab7a79ead3eadf4cb1d2
sjay05 <anusha.perumal@gmail.com>
2019-12-29 12:43:47 -0500

    2013 S5 Solved.

---
+++ 2013/S5.cpp
@@ -0,0 +1,34 @@
+// Iterative Approach 
+// Works on DMOJ and Official CCC Grader
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int cost = 0;
+    while (n >= 1) {
+        if (n == 1) {
+            cout << cost << endl;
+            break;
+        }
+        int i = n;
+        while (true) {
+            if (i == 1) break;
+            else {
+                if (n % i == 0 && i < n) break;
+                else i -= 1;
+            }
+        }
+        if (i == 1) {
+            n -= 1;
+            cost += n/1;
+        } else {
+            int ext = i;
+            n = n - ext;
+            cost += n/ext;
+        }
+    }
+}
---
+++ 2013/S5.py
@@ -0,0 +1,32 @@
+# Iterative Approach 
+# Works on Official CCC Grader but TLE (14th test case) on DMOJ
+
+
+n = input()
+
+cost = 0
+while n >= 1:
+    if n == 1:
+        print cost 
+        break
+    
+    i = n
+    while True:
+        if i == 1:
+            break
+        else:
+            if n % i == 0 and i < n:
+                break
+            else:
+                i -= 1 
+    
+    if i == 1:
+        n = n - 1
+        cost += (n/1)
+    else:
+        ext = i
+        n = n - ext
+        cost += (n/ext)
+        
+    
+            
