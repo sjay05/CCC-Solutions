631c63d884d785a8c3846e522cbb4383142da676
sjay05 <anusha.perumal@gmail.com>
2020-01-08 13:34:03 -0500

    2017 J5

---
+++ 2017/J5.py
@@ -0,0 +1,35 @@
+import sys
+
+N = input()
+arr = raw_input().split()
+arr = [int(i) for i in arr]
+
+# DP globals
+dp = [0] * 4002
+ma = [0] * 2001 
+
+for i in arr:
+    ma[i] += 1 
+
+def nailed_it_2():
+    for x in range(1, 2001):
+        for y in range(x, 2001):
+                if x == y:
+                    dp[x+y] += ma[x]/2
+                else:
+                    dp[x+y] += min(ma[x], ma[y])
+    
+    mh = 0
+    for i in dp:
+        if i > mh:
+            mh = i
+    
+    ff = 0
+    for i in dp:
+        if i == mh:
+          ff += 1
+          
+    print mh, ff
+    
+nailed_it_2()
+                
