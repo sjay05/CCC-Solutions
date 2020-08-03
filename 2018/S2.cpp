5dc3470dd057256414bfeb15c22731a9c74e6dff
DynamicSquid <60247838+DynamicSquid@users.noreply.github.com>
2020-08-03 16:34:45 -0600

    Create S2.cpp

---
+++ 2018/S2.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	vector<vector<int> > grid(N);
+	for (int a = 0; a < N; ++a)
+	{
+		for (int b = 0; b < N; ++b)
+		{
+			int val;
+			cin >> val;
+
+			grid[a].push_back(val);
+		}
+	}
+
+	if (grid[0][0] < grid[0][1] && grid[0][0] < grid[1][0])
+	{
+		for (int a = 0; a < N; ++a)
+		{
+			for (int b = 0; b < N; ++b)
+				std::cout << grid[a][b] << ' ';
+			std::cout << '\n';
+		}
+	}
+	else if (grid[0][0] > grid[0][1] && grid[0][0] < grid[1][0])
+	{
+		for (int a = N - 1; a >= 0; --a)
+		{
+			for (int b = 0; b < N; ++b)
+				std::cout << grid[b][a] << ' ';
+			std::cout << '\n';
+		}
+	}
+	else if (grid[0][0] > grid[0][1] && grid[0][0] > grid[1][0])
+	{
+		for (int a = N - 1; a >= 0; --a)
+		{
+			for (int b = N - 1; b >= 0; --b)
+				std::cout << grid[a][b] << ' ';
+			std::cout << '\n';
+		}
+	}
+	else
+	{
+		for (int a = 0; a < N; ++a)
+		{
+			for (int b = N - 1; b >= 0; --b)
+				std::cout << grid[b][a] << ' ';
+			std::cout << '\n';
+		}
+	}
+}
