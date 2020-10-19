d58fb6ad7750c8ca931b4be19592f5a4b7d0366e
sjay05 <878296@pdsb.net>
2020-10-19 12:46:39 -0400

    mods

--- 2019/S3.cpp
+++ 2019/S3.cpp
@@ -36,19 +36,17 @@ void shuffle(vector<T> &a) {
 
 using namespace rng_distribution;
 
-template <class T>
-vector<T> create(size_t size, T initialValue) {
-  return vector<T>(size, initialValue);
-}
+vector <vector <int> > arr = {
+  {0, 0, 0},
+  {0, 0, 0},
+  {0, 0, 0},
+};
 
-template <class T, class... Args>
-auto create(size_t head, Args&&... tail) {
-  auto inner = create<T>(tail...);
-  return vector<decltype(inner)>(head, inner);
-}
-
-auto arr = create<int>(3, 3, 0);
-auto bad = create<int>(3, 3, 0);
+vector <vector <int> > bad = {
+  {0, 0, 0},
+  {0, 0, 0},
+  {0, 0, 0},
+}; 
 
 int main() {  
   ios::sync_with_stdio(false);
