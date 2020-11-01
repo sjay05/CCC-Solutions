99d9f79b67f53352bc36584b9eae8e7d9029a358
sjay05 <878296@pdsb.net>
2020-11-01 11:38:18 -0500

    add 2009 s4

--- 2009/S4.cpp
+++ 2009/S4.cpp
@@ -1,67 +1,105 @@
-/**
- *    author:  sjay05
-**/
-/*
-TLE - 20/100 (Doesn't Pass All Test Cases)
-*/
 #include <bits/stdc++.h>
 
 using namespace std;
 
 typedef long long ll;
-typedef pair<int,int> pii;
 
-const int INF = 0x3f3f3f3f;
-const int MXN = 5050;
+template <typename T>
+class graph {
+  public:
+  struct edge {
+    int from, to;
+    T cost;
+  };
+  
+  vector <edge> edges;
+  vector <vector <int> > g;
+  int n;
 
-int n, t, k, d;
-map<int,int> pen;
+  graph(int _n) : n(_n) {
+    g.reserve(n);
+    g.resize(n);
+  }
 
-int adj[MXN][MXN];
-vector<int> dist(MXN, INF);
-vector<bool> vis(MXN, false);
+  virtual void add(int from, int to, T cost) = 0;
+};
 
-void dijk(int u) {
-    vis[u] = true; // mark true
-    for (int _m = 1; _m <= n; _m++) {
-        if (!vis[_m]) {
-            if (dist[u] + adj[u][_m] < dist[_m])
-                // only if < inf
-                dist[_m] = dist[u] + adj[u][_m];
-        }
+template <typename T>
+class undigraph : public graph<T> {
+  public:
+  using graph<T>::n;
+  using graph<T>::g;
+  using graph<T>::edge;
+  using graph<T>::edges;
+
+  undigraph(int n) : graph<T>(n) {
+  }
+
+  void add(int from, int to, T cost = -1) {
+    assert(0 <= from && from < n && 0 <= to && to < n);
+    int id = (int) edges.size();
+    g[from].push_back(id);
+    g[to].push_back(id);
+    edges.push_back({from, to, cost});
+  }
+};
+
+template <typename T>
+vector<T> dijkstra(const graph<T> &g, int start) {
+  assert(0 <= start && start < g.n);
+  vector<T> dist(g.n, numeric_limits<T>::max());
+  priority_queue<pair<T, int>, vector<pair<T, int> >, greater<pair<T, int> > > s;
+  dist[start] = 0;
+  s.emplace(dist[start], start);
+  while (!s.empty()) {
+    T expected = s.top().first;
+    int i = s.top().second;
+    s.pop();
+    if (dist[i] != expected) {
+      continue;
     }
+    for (int id : g.g[i]) {
+      auto &e = g.edges[id];
+      int to = e.from ^ e.to ^ i;
+      if (dist[i] + e.cost < dist[to]) {
+        dist[to] = dist[i] + e.cost;
+        s.emplace(dist[to], to);
+      }
+    }
+  }
+  return dist;
+}  
+
+int main() {  
+  ios::sync_with_stdio(false);
+  cin.tie(0); 
+  int n, t;
+  cin >> n >> t;
+  undigraph<int> g(n);
+  for (int i = 0; i < t; i++) {
+    int x, y, c;
+    cin >> x >> y >> c;
+    --x; --y;
+    g.add(x, y, c);
+  }
+  int k;
+  cin >> k;
+  vector<pair<int, int>> pen(k);
+  for (int i = 0; i < k; i++) {
+    cin >> pen[i].first >> pen[i].second;
+    --pen[i].first;
+  }                                   
+  int d;
+  cin >> d;
+  vector<int> dist = dijkstra(g, --d);
+  const int inf = numeric_limits<int>::max();
+  int ans = inf;  
+  for (int i = 0; i < k; i++) {
+    int cost = pen[i].second;
+    cost += dist[pen[i].first];
+    ans = min(ans, cost);
+  }
+  cout << ans << '\n';
+  return 0;
 }
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    cin >> n >> t;
-    for (int i = 0; i < t; i++) {
-        int x, y, co; cin >> x >> y >> co;
-        // bi-direct
-        adj[x][y] = co;
-        adj[y][x] = co;
-    }
-    cin >> k;
-    for (int i = 0; i < k; i++) {
-        int z, p_z; cin >> z >> p_z;
-        // assign map
-        pen[z] = p_z;
-    }
-    cin >> d; dist[d] = 0;
-    for (int i = 0; i < n; i++) {
-        int u_v = 0; // pick arbitrary node unvis
-        for (int no = 1; no <= n; no++) {
-            if (!vis[no] && dist[no] < dist[u_v])
-                // not vis and less dist (!inf)
-                u_v = no;
-        }
-        dijk(u_v);
-    }
-    int min_d = dist[d] + pen[d];
-    for (int x = 1; x <= n; x++) {
-        min_d = min(min_d, dist[x]+pen[x]);
-    }
-    cout << min_d << "\n";
-}
