0f790dcdb57bb38524161453dc2bfaf42f5fc7cb
sjay05 <878296@pdsb.net>
2020-12-30 17:21:43 -0500

    mods

--- 2014/S1.cpp
+++ 2014/S1.cpp
@@ -1,37 +1,34 @@
 /*
  * Author: DynamicSquid
  */
- 
+
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-int main()
-{
-    int K, m;
-    cin >> K >> m;
+int main() {
+  int K, m;
+  cin >> K >> m;
 
-    vector<int> people(K);
+	vector<int> people(K);
 
-    for (int a = 0; a < K; ++a)
-        people[a] = a + 1;
+  for (int a = 0; a < K; ++a)
+    people[a] = a + 1;
 
-    for (int a = 0; a < m; ++a)
-    {
-        int num;
-        cin >> num;
+  for (int a = 0; a < m; ++a) {
+    int num;
+    cin >> num;
 
-        vector<int> temp;
-        for (size_t b = 0; b < people.size(); ++b)
-        {
-            if ((b + 1) % num != 0)
-                temp.push_back(people[b]);
-        }
-
-        people = temp;
+    vector<int> temp;
+    for (size_t b = 0; b < people.size(); ++b) {
+      if ((b + 1) % num != 0)
+        temp.push_back(people[b]);
     }
 
-    for (size_t a = 0; a < people.size(); ++a)
-        cout << people[a] << '\n';
+    people = temp;
+  }
+
+  for (size_t a = 0; a < people.size(); ++a)
+    cout << people[a] << '\n';
 }
--- 2014/S3.cpp
+++ 2014/S3.cpp
@@ -1,64 +1,50 @@
+/*
+Author: DynamicSquid
+*/
+
 #include <iostream>
 #include <deque>
 
 using namespace std;
 
-int main()
-{
-    int T;
-    cin >> T;
+int main() {
+  int T;
+  cin >> T;
 
-    deque<char> output;
+  deque<char> output;
 
-    for (int a = 0; a < T; ++a)
-    {
-        int N;
-        cin >> N;
-
-        deque<int> cars;
-
-        for (int b = 0; b < N; ++b)
-        {
-            int num;
-            cin >> num;
-
-            cars.push_front(num);
-        }
-
-        deque<int> branch;
-
-        int curr = 1;
-
-        for (size_t b = 0; b < cars.size(); ++b)
-        {
-            if (cars[b] == curr)
-            {
-                curr += 1;
-            }
-            else if (!branch.empty() && branch.front() == curr)
-            {
-                branch.pop_front();
-                curr += 1;
-                b -= 1;
-            }
-            else
-            {
-                branch.push_front(cars[b]);
-            }
-        }
-
-        for (size_t b = 0; b < branch.size(); ++b)
-        {
-            if (branch[b] == curr)
-                curr += 1;
-        }
-
-        if (curr - 1 != N)
-            output.push_back('N');
-        else
-            output.push_back('Y');
+  for (int a = 0; a < T; ++a) {
+    int N;
+    cin >> N;
+    deque<int> cars;
+    for (int b = 0; b < N; ++b) {
+      int num;
+      cin >> num;
+      cars.push_front(num);
     }
+    deque<int> branch;
+    int curr = 1;
+    for (int b = 0; b < cars.size(); ++b) {
+      if (cars[b] == curr) {
+        curr += 1;
+      } else if (!branch.empty() && branch.front() == curr) {
+        branch.pop_front();
+        curr += 1;
+        b -= 1;
+      } else {
+        branch.push_front(cars[b]);
+      }
+    }
+    for (size_t b = 0; b < branch.size(); ++b) {
+      if (branch[b] == curr)
+        curr += 1;
+    }
+    if (curr - 1 != N)
+      output.push_back('N');
+    else
+      output.push_back('Y');
+  }
 
-    for (size_t a = 0; a < output.size(); ++a)
-        cout << output[a] << '\n';
+  for (size_t a = 0; a < output.size(); ++a)
+    cout << output[a] << '\n';
 }
